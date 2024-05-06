1bfb94c5c8b2c898903d787118cc6b4701d38ada
Abhishek lama <Abhisheklama@visions.net.in>
2024-05-06 16:09:50 +0530

    changes

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -3,34 +3,46 @@ import {
   InteractionType,
 } from "@azure/msal-browser";
 import { useMsal, useMsalAuthentication } from "@azure/msal-react";
+import { useEffect } from "react";
 
 function Login() {
   const { login } = useMsalAuthentication(InteractionType.Popup);
 
   const { instance, inProgress, accounts, logger } = useMsal();
-  console.log("instance", instance, inProgress, accounts, logger);
-  // Get access token for the first account
-  if (inProgress == "login") {
-    const accessTokenRequest = {
-      scopes: ["user.read"], // Scopes required for your API
-    };
-    console.log("before token");
-    instance
-      .acquireTokenSilent(accessTokenRequest)
-      .then((token) => {
-        console.log("token", token);
-        // Do something with the tokenResponse
-      })
-      .catch(async (error) => {
-        if (error instanceof InteractionRequiredAuthError) {
-          // fallback to interaction when silent call fails
-          let token = instance.acquireTokenPopup(accessTokenRequest);
-          console.log("token popup", token);
-        }
-        console.log("error", error);
-        // handle other errors
-      });
-  }
+  console.log(
+    "instance",
+    instance,
+    "inProgress",
+    inProgress,
+    "accounts",
+    accounts,
+    "logger",
+    logger
+  );
+  useEffect(() => {
+    // Get access token for the first account
+    if (inProgress == "login") {
+      const accessTokenRequest = {
+        scopes: ["user.read"], // Scopes required for your API
+      };
+      console.log("before token");
+      instance
+        .acquireTokenSilent(accessTokenRequest)
+        .then((token) => {
+          console.log("token", token);
+          // Do something with the tokenResponse
+        })
+        .catch(async (error) => {
+          if (error instanceof InteractionRequiredAuthError) {
+            // fallback to interaction when silent call fails
+            let token = instance.acquireTokenPopup(accessTokenRequest);
+            console.log("token popup", token);
+          }
+          console.log("error", error);
+          // handle other errors
+        });
+    }
+  }, [inProgress]);
   return <button onClick={() => login()}>Sign In</button>;
 }
 export default Login;
