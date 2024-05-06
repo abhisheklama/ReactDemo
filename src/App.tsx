22ea1ee9a0acc788ab8be71ca2e5db8163bcd33c
Abhishek lama <Abhisheklama@visions.net.in>
2024-05-06 17:18:32 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,61 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Login from "./components/login";
+import { useMsal } from "@azure/msal-react";
+import {
+  InteractionRequiredAuthError,
+  InteractionType,
+} from "@azure/msal-browser";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const { instance, inProgress, accounts } = useMsal();
+  const [Token, setToken] = useState("");
+  useEffect(() => {
+    // Get access token for the first account
+    if (inProgress == "none") {
+      instance.setActiveAccount(accounts[0]);
+      const accessTokenRequest = {
+        scopes: ["User.read"], // Scopes required for your API
+      };
+      console.log("before token");
+      instance
+        .acquireTokenSilent(accessTokenRequest)
+        .then((token) => {
+          console.log("token", token);
+          setToken(token.accessToken);
+
+          axios
+            .get("https://graph.microsoft.com/v1.0/me", {
+              headers: {
+                Authorization: `Bearer ${token.accessToken}`,
+              },
+            })
+            .then((res) => console.log("profile", res));
+          axios
+            .get("https://graph.microsoft.com/v1.0/me/photo/$value", {
+              headers: {
+                Authorization: `Bearer ${token.accessToken}`,
+              },
+            })
+            .then((res) => console.log("photo", res));
+          // Do something with the tokenResponse
+        })
+        .catch(async (error) => {
+          if (error instanceof InteractionRequiredAuthError) {
+            // fallback to interaction when silent call fails
+            let token = instance.acquireTokenPopup(accessTokenRequest);
+            console.log("token popup", token);
+          }
+          console.log("error", error);
+          // handle other errors
+        });
+    }
+  }, [inProgress]);
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <h1>Demo App</h1>
+    </>
   );
 }
 
 export default App;
-
-const Home = () => {
-  return (
-    <>
-      <h1>Demo App</h1>
-      <Login />
-    </>
-  );
-};
