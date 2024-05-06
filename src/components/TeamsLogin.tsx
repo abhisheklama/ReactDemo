3a78d623ec96def147e5415bb63f298741608d44
Abhishek lama <Abhisheklama@visions.net.in>
2024-05-06 15:30:50 +0530

    changes

--- src/components/TeamsLogin.tsx
+++ src/components/TeamsLogin.tsx
@@ -1,54 +1,54 @@
-import {
-  TeamsUserCredentialAuthConfig,
-  TeamsUserCredential,
-} from "@microsoft/teamsfx";
-import { Client } from "@microsoft/microsoft-graph-client";
-import { TokenCredentialAuthenticationProvider } from "@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials";
+// import {
+//   TeamsUserCredentialAuthConfig,
+//   TeamsUserCredential,
+// } from "@microsoft/teamsfx";
+// import { Client } from "@microsoft/microsoft-graph-client";
+// import { TokenCredentialAuthenticationProvider } from "@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials";
 
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 const TeamsLogin = () => {
-  useEffect(() => {
-    const authConfig: TeamsUserCredentialAuthConfig = {
-      clientId: "xxx",
-      initiateLoginEndpoint: "https://react-demo-pied.vercel.app/login",
-    };
+  // useEffect(() => {
+  //   const authConfig: TeamsUserCredentialAuthConfig = {
+  //     clientId: "xxx",
+  //     initiateLoginEndpoint: "https://react-demo-pied.vercel.app/login",
+  //   };
 
-    const teamsUserCredential = new TeamsUserCredential(authConfig);
-    console.log("teamsUserCredential", teamsUserCredential);
+  //   const teamsUserCredential = new TeamsUserCredential(authConfig);
+  //   console.log("teamsUserCredential", teamsUserCredential);
 
-    teamsUserCredential
-      .login("User.Read")
-      .then(() => {
-        console.log("fetching  token...");
-        teamsUserCredential
-          .getToken("Personal")
-          .then((tokenResponse) => {
-            console.log("ss", tokenResponse);
-            console.log("inside login fn!");
+  //   teamsUserCredential
+  //     .login("User.Read")
+  //     .then(() => {
+  //       console.log("fetching  token...");
+  //       teamsUserCredential
+  //         .getToken("Personal")
+  //         .then((tokenResponse) => {
+  //           console.log("ss", tokenResponse);
+  //           console.log("inside login fn!");
 
-            const authProvider = new TokenCredentialAuthenticationProvider(
-              teamsUserCredential,
-              {
-                scopes: ["User.Read"],
-              }
-            );
-            console.log("authProvider", authProvider);
+  //           const authProvider = new TokenCredentialAuthenticationProvider(
+  //             teamsUserCredential,
+  //             {
+  //               scopes: ["User.Read"],
+  //             }
+  //           );
+  //           console.log("authProvider", authProvider);
 
-            const graphClient = Client.initWithMiddleware({
-              authProvider: authProvider,
-            });
-            graphClient
-              .api("/me")
-              .get()
-              .then((profile) => {
-                console.log(profile);
-              });
-          })
-          .catch((err) => console.log("token err", err));
-      })
-      .catch((err) => console.log("login err", err));
-  }, []);
+  //           const graphClient = Client.initWithMiddleware({
+  //             authProvider: authProvider,
+  //           });
+  //           graphClient
+  //             .api("/me")
+  //             .get()
+  //             .then((profile) => {
+  //               console.log(profile);
+  //             });
+  //         })
+  //         .catch((err) => console.log("token err", err));
+  //     })
+  //     .catch((err) => console.log("login err", err));
+  // }, []);
   return <div>Teams</div>;
 };
 
