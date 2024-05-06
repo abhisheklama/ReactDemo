42dd28cb9458956726ac046c85b4c9f5a48a5a69
Abhishek lama <Abhisheklama@visions.net.in>
2024-05-06 15:23:58 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import TeamsLogin from "./components/TeamsLogin";
 import Login from "./components/login";
 
 function App() {
