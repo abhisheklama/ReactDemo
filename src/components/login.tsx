32972b85e05e772f9255d6ff93aa1dcc10a1f2ef
Abhishek lama <Abhisheklama@visions.net.in>
2024-05-06 15:36:28 +0530

    changes

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,8 +1,20 @@
 import { InteractionType } from "@azure/msal-browser";
-import { useMsalAuthentication } from "@azure/msal-react";
+import { useMsal, useMsalAuthentication } from "@azure/msal-react";
 
 function Login() {
   const { login } = useMsalAuthentication(InteractionType.Popup);
+
+  const { instance } = useMsal();
+  const accounts = instance.getAllAccounts();
+  console.log("accounts", accounts);
+  // Get access token for the first account
+  const accessTokenRequest = {
+    scopes: ["user.read"], // Scopes required for your API
+  };
+
+  instance
+    .acquireTokenSilent(accessTokenRequest)
+    .then((token) => console.log("tokeb", token));
 
   return <button onClick={() => login()}>Sign In</button>;
 }
