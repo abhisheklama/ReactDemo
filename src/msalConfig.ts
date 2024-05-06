3817ffc2916972033583f3c5416e06dfc43f4849
Abhishek lama <Abhisheklama@visions.net.in>
2024-05-06 15:25:59 +0530

    changes

--- src/msalConfig.ts
+++ src/msalConfig.ts
@@ -1,8 +1,9 @@
 // msalConfig.js
 export const msalConfig = {
   auth: {
-    clientId: "your_client_id",
-    authority: "https://login.microsoftonline.com/your_tenant_id",
-    redirectUri: "http://localhost:3000", // Update with your redirect URI
+    clientId: "c873c02f-c54c-4ef0-82f2-ca953957b0b7",
+    authority:
+      "https://login.microsoftonline.com/9d143c90-308b-45c4-926a-c972e2f01269",
+    redirectUri: "https://react-demo-pied.vercel.app/", // Update with your redirect URI
   },
 };
