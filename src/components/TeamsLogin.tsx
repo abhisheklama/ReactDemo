e7ec3d2b9c30a4b9637d4500fe1176450e28a302
Abhishek lama <Abhisheklama@visions.net.in>
2024-05-06 11:26:07 +0530

    changes

--- src/components/TeamsLogin.tsx
+++ src/components/TeamsLogin.tsx
@@ -25,7 +25,7 @@ const TeamsLogin = () => {
     );
     console.log("authProvider", authProvider);
     teamsUserCredential
-      .getToken("Personal")
+      .getToken("")
       .then((token) => {
         console.log("token", token);
       })
