4001ddf1e4f3ad2c8c0dedb311c0635780f17e31
Abhishek lama <Abhisheklama@visions.net.in>
2024-05-06 15:57:22 +0530

    changes

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,12 +1,13 @@
-import { InteractionType } from "@azure/msal-browser";
+import {
+  InteractionRequiredAuthError,
+  InteractionType,
+} from "@azure/msal-browser";
 import { useMsal, useMsalAuthentication } from "@azure/msal-react";
 
 function Login() {
   const { login } = useMsalAuthentication(InteractionType.Popup);
 
   const { instance } = useMsal();
-  const accounts = instance.getAllAccounts();
-  console.log("accounts", accounts);
   // Get access token for the first account
   const accessTokenRequest = {
     scopes: ["user.read"], // Scopes required for your API
@@ -14,8 +15,18 @@ function Login() {
 
   instance
     .acquireTokenSilent(accessTokenRequest)
-    .then((token) => console.log("tokeb", token));
+    .then((token) => {
+      console.log("token", token);
+      // Do something with the tokenResponse
+    })
+    .catch(async (error) => {
+      if (error instanceof InteractionRequiredAuthError) {
+        // fallback to interaction when silent call fails
+        return instance.acquireTokenPopup(accessTokenRequest);
+      }
 
+      // handle other errors
+    });
   return <button onClick={() => login()}>Sign In</button>;
 }
 export default Login;
