a25e4319822025c215739f92fa275f0c26c922f0
Abhishek lama <Abhisheklama@visions.net.in>
2024-05-07 12:20:20 +0530

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 import { MsalProvider } from "@azure/msal-react";
 import { PublicClientApplication } from "@azure/msal-browser";
 import { msalConfig } from "./msalConfig.ts";
