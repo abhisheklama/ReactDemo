7243ff3a24e2300c94d12519efe2b5e40a6b0b49
Abhishek lama <Abhisheklama@visions.net.in>
2024-05-07 18:12:03 +0530

    changes

--- src/components/AuthEnd.tsx
+++ src/components/AuthEnd.tsx
@@ -5,7 +5,7 @@ const AuthEnd = () => {
   console.log("clientId", clientId);
   const msalConfig = {
     auth: {
-      clientId: clientId + "",
+      clientId: "c873c02f-c54c-4ef0-82f2-ca953957b0b7",
       authority:
         "https://login.microsoftonline.com/9d143c90-308b-45c4-926a-c972e2f01269",
       navigateToLoginRequestUrl: false,
