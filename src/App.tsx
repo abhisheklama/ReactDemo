c9f555cb420b796a579e59e776269470b0c5e178
Abhishek lama <Abhisheklama@visions.net.in>
2024-05-07 14:20:50 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import { InteractionRequiredAuthError } from "@azure/msal-browser";
 import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { TokenContext } from "./main";
+import {
+  TeamsUserCredential,
+  TeamsUserCredentialAuthConfig,
+} from "@microsoft/teamsfx";
 
 function App() {
   const msal = useMsal();
@@ -14,6 +18,17 @@ function App() {
   console.log("tokenContext", tokenContext);
   useEffect(() => {
     console.log("msal", msal);
+    const authConfig: TeamsUserCredentialAuthConfig = {
+      clientId: "c873c02f-c54c-4ef0-82f2-ca953957b0b7",
+      initiateLoginEndpoint:
+        "https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize",
+    };
+
+    const teamsUserCredential = new TeamsUserCredential(authConfig);
+
+    // Put these code in a call-to-action callback function to avoid browser blocking automatically showing up pop-ups.
+    (async () => await teamsUserCredential.login(["User.Read"]))(); // Login with scope
+
     // Get access token for the first account
     let token = localStorage.getItem("token");
     console.log("token", token);
@@ -75,7 +90,7 @@ function App() {
           });
       }
     }
-  }, [inProgress]);
+  }, []);
   return (
     <>
       {!profile ? (
