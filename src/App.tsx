aca5f424aa5ab5d6a9ff1093d0d07e15eb826b64
Abhishek lama <Abhisheklama@visions.net.in>
2024-05-07 11:12:48 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
     if (inProgress == "none") {
       instance.setActiveAccount(accounts[0]);
       const accessTokenRequest = {
-        scopes: ["User.read", "User.read.all"], // Scopes required for your API
+        scopes: ["User.Read", "User.Read.All", "User.ReadBasic.All"], // Scopes required for your API
       };
       console.log("before token");
       instance
