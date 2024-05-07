e4e63f0c7e60256b0e3792138d206d0e8ae2ce0d
Abhishek lama <Abhisheklama@visions.net.in>
2024-05-07 16:23:25 +0530

    changes

--- src/components/TeamsLogin.tsx
+++ src/components/TeamsLogin.tsx
@@ -1,3 +1,4 @@
+import { PublicClientApplication } from "@azure/msal-browser";
 import { useSearchParams } from "react-router-dom";
 
 const TeamsLogin = () => {
@@ -6,18 +7,23 @@ const TeamsLogin = () => {
   const clientId = query.get("clientId");
   console.log("clientId", clientId);
 
-  // const msalConfig = {
-  //   auth: {
-  //     clientId: "c873c02f-c54c-4ef0-82f2-ca953957b0b7",
-  //     authority:
-  //       "https://login.microsoftonline.com/9d143c90-308b-45c4-926a-c972e2f01269",
-  //     redirectUri: "https://react-demo-pied.vercel.app/", // Update with your redirect URI
-  //   },
-  //   cache: {
-  //     cacheLocation: "sessionStorage", // This configures where your cache will be stored
-  //     storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
-  //   },
-  // };
+  const msalConfig = {
+    auth: {
+      clientId: clientId + "",
+      authority:
+        "https://login.microsoftonline.com/9d143c90-308b-45c4-926a-c972e2f01269",
+      redirectUri: "https://react-demo-pied.vercel.app/", // Update with your redirect URI
+    },
+    cache: {
+      cacheLocation: "sessionStorage", // This configures where your cache will be stored
+      storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    },
+  };
+
+  const msal = new PublicClientApplication(msalConfig);
+
+  (async () => await msal.initialize())();
+  msal.loginRedirect();
 
   return <>Login</>;
 };
