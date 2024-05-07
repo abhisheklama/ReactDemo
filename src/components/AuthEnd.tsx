0e2c807fc3268cdbf9184e8c7c6aeb3439416c75
Abhishek lama <Abhisheklama@visions.net.in>
2024-05-07 17:57:29 +0530

    changes

--- src/components/AuthEnd.tsx
+++ src/components/AuthEnd.tsx
@@ -1,20 +1,19 @@
 import { PublicClientApplication } from "@azure/msal-browser";
-import { useSearchParams } from "react-router-dom";
+// import { useSearchParams } from "react-router-dom";
 
 const AuthEnd = () => {
-  const [query] = useSearchParams();
-  console.log("query", query);
-  const clientId = query.get("clientId");
+  //   const [query] = useSearchParams();
+  //   console.log("query", query);
+  //   const clientId = query.get("clientId");
   const msalConfig = {
     auth: {
-      clientId: clientId + "",
+      clientId: "c873c02f-c54c-4ef0-82f2-ca953957b0b7",
       authority:
         "https://login.microsoftonline.com/9d143c90-308b-45c4-926a-c972e2f01269",
       navigateToLoginRequestUrl: false,
     },
     cache: {
       cacheLocation: "sessionStorage", // This configures where your cache will be stored
-      storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
     },
   };
 
